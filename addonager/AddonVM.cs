77a3210c52304179d49816ac6f9ae2f14b6b8eda
Brian Donaldson <nairbsd@gmail.com>
2020-07-09 21:16:37 -0500

    Add wpf proj. Add MahApps framework.

---
+++ addonager/AddonVM.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace addonager
+{
+    public class AddonVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Interface
+        //Specifies which client interface version the addon was made for. If the value of this tag does not match the client interface version, the addon is loaded only if the "Load out of date addons" option is enabled.There are a number of ways to determine the current interface version.
+        //## Interface: 80205
+        private string _interface { get; set; }
+        public string Interface
+        {
+            get { return _interface; }
+            set
+            {
+                _interface = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+        //Title
+        //The value of this tag is displayed in the AddOns list.Localized versions can be included by appending a hyphen followed by the client locale name; the client automatically chooses a localized version if one is available.The value may also contain UI escape sequences, such as for example colors.
+        //## Title: Waiting for Godot
+        //## Title-frFR: En attendant Godot
+        private string _title { get; set; }
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+        //Version
+        //The AddOn version.Some automatic updating tools may prefer that this string begins with a numeric version number.
+        private string _version { get; set; }
+        public string Version
+        {
+            get { return _version; }
+            set
+            {
+                _version = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+
+        //Notes
+        //Addon description that appears when the user hovers over the addon entry in the addons list.Like Title, this tag can be localized by appending a hyphen followed by locale name, and contain UI escape sequences.
+        //## Notes: "Nothing to be done"
+
+        //RequiredDeps, Dependencies, or any word beginning with "Dep"
+        //A comma-separated list of addon (directory) names that must be loaded before this addon can be loaded.
+        // ## Dependencies: someAddOn, someOtherAddOn
+
+        //OptionalDeps
+        //A comma-separated list of addon (directory) names that should be loaded before this addon if they can be loaded.
+        //## OptionalDeps: someAddOn, someOtherAddOn
+
+        //LoadOnDemand
+        //If the value of this tag is "1", the addon is not loaded when the user first logs in, but can be loaded by another addon at a later point.This can be used to avoid loading situational addons.
+        //## LoadOnDemand: 1
+
+        //LoadWith
+        //A comma-separated list of addon (directory) names that, when loaded, will cause the client to load this LoadOnDemand addon as well.Added in Patch 1.9
+        //## LoadWith: someAddOn, someOtherAddOn
+
+        //LoadManagers
+        //A comma-separated list of addon (directory) names; if no addons on this list are loaded, the client will load your addon when the user logs in; if at least one addon on this list is loaded, your addon is treated as LoadOnDemand.Introduced in patch 2.1; an example of a LoadManager is AddonLoader.
+
+        //SavedVariables
+        //A comma-separated list of variable names in the global environment that will be saved to disk when the client exits, and placed back into the global environment when your addon is loaded; the variables are not available before the ADDON_LOADED event fires for your addon. See Saving variables between game sessions.
+        //## SavedVariables: foo, bar
+
+        //SavedVariablesPerCharacter
+        //A comma-separated list of variable names in the global environment that will be saved to disk when the client exits, and placed back into the global environment when your addon is loaded for a particular character. Note that PerCharacter saved variables are only loaded for the character for which they were saved.
+        //## SavedVariablesPerCharacter: somePercharVariable
+
+        //DefaultState
+        //Determines whether the addon is enabled by default when first installed. If the value of this tag is "disabled", the user must explicitly enable the addon in the addons list before it is loaded.
+        //## DefaultState: enabled
+
+        //Secure
+        //If the value of this tag is 1, and the addon is digitally signed by Blizzard, its code is considered secure (as opposed to tainted, which is what all 3rd-party addons are). Introduced in Patch 1.11.
+
+        //Author
+        //The AddOn author's name
+
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string caller = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
+    }
+}
---
+++ addonager/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace addonager
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public ObservableCollection<AddonVM> ListOfAddons
+        {
+            get { return _listOfAddons; }
+            set
+            {
+                if (_listOfAddons != value)
+                {
+                    _listOfAddons = value;
+                }
+            }
+        }
+
+        private ObservableCollection<AddonVM> _listOfAddons { get; set; } = new ObservableCollection<AddonVM>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private void LaunchGitHubSite(object sender, RoutedEventArgs e)
+        {
+            var tacos = Directory.EnumerateDirectories(@"C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns").ToList();
+
+            foreach (var iteam in tacos)
+            {
+                _listOfAddons.Add(new AddonVM() { Name = iteam, Version="0.0.0"});
+            }
+        }
+
+        private void DeployCupCakes(object sender, RoutedEventArgs e)
+        {
+            //C:\Program Files (x86)\World of Warcraft\_retail_\Interface
+
+            //secondFlyout.IsOpen = !secondFlyout.IsOpen;
+            //await this.ShowMessageAsync("This is the title", "Some message");
+        }
+    }
+
+    public enum Position
+    {
+        Left,
+        Right,
+        Top,
+        Bottom
+    }
+
+    public enum FlyoutTheme
+    {
+        /// <summary>
+        /// Adapts the Flyout's theme to the theme of its host window.
+        /// </summary>
+        Adapt,
+
+        /// <summary>
+        /// Adapts the Flyout's theme to the theme of its host window, but inverted.
+        /// This theme can only be applied if the host window's theme abides the "Dark" and "Light" affix convention.
+        /// (see <see cref="ThemeManager.GetInverseTheme"/> for more infos.
+        /// </summary>
+        Inverse,
+
+        /// <summary>
+        /// The dark theme. This is the default theme.
+        /// </summary>
+        Dark,
+        Light,
+
+        /// <summary>
+        /// The flyouts theme will match the host window's accent color.
+        /// </summary>
+        Accent
+    }
+
+    [Flags]
+    public enum WindowCommandsOverlayBehavior
+    {
+        /// <summary>
+        /// Doesn't overlay a hidden TitleBar.
+        /// </summary>
+        Never = 0,
+
+        /// <summary>
+        /// Overlays a hidden TitleBar.
+        /// </summary>
+        HiddenTitleBar = 1 << 0
+    }
+
+    [Flags]
+    public enum OverlayBehavior
+    {
+        /// <summary>
+        /// Doesn't overlay Flyouts nor a hidden TitleBar.
+        /// </summary>
+        Never = 0,
+
+        /// <summary>
+        /// Overlays opened <see cref="Flyout"/> controls.
+        /// </summary>
+        Flyouts = 1 << 0,
+
+        /// <summary>
+        /// Overlays a hidden TitleBar.
+        /// </summary>
+        HiddenTitleBar = 1 << 1,
+
+        Always = ~(-1 << 2)
+    }
+}
